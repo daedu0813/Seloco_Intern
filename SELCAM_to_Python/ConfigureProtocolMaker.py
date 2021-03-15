a8ab017d664b50bb9a9a3893cfa69692ebdd272f
DAEDUKIM <daedu0813@gmail.com>
2021-03-16 00:01:31 +0900

    210315_2 ConfigureProtocolMaker.py 수정중

--- SELCAM_to_Python/ConfigureProtocolMaker.py
+++ SELCAM_to_Python/ConfigureProtocolMaker.py
@@ -5,9 +5,9 @@ class ConfigureProtocolMaker:
         return 0
     
     def makeSearchMessage(HW_name):
-        message = []
-        tempbuff = []
-        protocol = []
+        message = bytes(20)
+        tempbuff = bytes()
+        protocol = bytes(10)
         
         #SOF    [VB에서는 '&H' Python에서는 '0x']
         message[0] = 0xFF
@@ -49,17 +49,141 @@ class ConfigureProtocolMaker:
         makeSearchMessage = message
 
     def makeNetworkSettingMessage(HW_name, IP, subnetmask, gateway, port):
-        message = []
-        tempbuff = []
-        protocol = []
+        message = bytes(38)
+        tempbuff = bytes()
+        protocol = bytes(10)
+        pos = 0
 
         #SOF
         message[0] = 0xFF
         message[1] = 0x53
 
-        #pos=2
+        pos = 2
         #Potocol version '0001'
         protocol = StringToBytes(__Protocol_Ver)    #Protocol verion length 4
+        message[2] = protocol[0]
+        message[3] = protocol[1]
+        message[4] = protocol[2]
+        message[5] = protocol[3]
+
+        pos = 6
+        #Data length
+        message[6] = 0
+        message[7] = 26
+
+        pos = 8
+        #Service Type
+        message[8] = 0x21 #Tool to device
+        #Service ID
+        message[9] = 0xA1 #Set IP command
+
+        pos = 10
+        #Data get all the node info
+        tempbuff = StringToBytes(HW_name)   # HW Name length 8
+        print('HW Name' + str(len(tempbuff)))
+        for i in range(0, len(tempbuff) -2):
+            message[pos] = tempbuff[i]
+            pos = pos + 1
+        print('hw pos' + str(pos))
+
+        message[pos] = 0x25 # %
+        pos = pos + 1
+
+        tempbuff = StringToBytes(IP)
+        tempbuff = __IPto4B(tempbuff)
+        for i in range(0, len(tempbuff) -2):
+            message[pos] = tempbuff[i]
+            pos = pos + 1
+        print('ip pos ' + str(pos))
+        message[pos] = 0x25 # %
+        pos = pos + 1
+
+        tempbuff = StringToBytes(subnetmask)
+        tempbuff = __IPto4B(tempbuff)
+        for i in range(0, len(tempbuff) -2):
+            message[pos] = tempbuff[i]
+            pos = pos + 1
+        print('sub pos ' + str(pos))
+        message[pos] = 0x25 # %
+        pos = pos + 1
+
+        tempbuff = StringToBytes(gateway)
+        tempbuff = __IPto4B(tempbuff)
+        for i in range(0, len(tempbuff) -2):
+            message[pos] = tempbuff[i]
+            pos = pos + 1
+        print('gw pos ' + str(pos))
+        message[pos] = 0x25 # %
+        pos = pos + 1 
+
+        message[pos] = port // 256
+        message[pos + 1] = port % 256
+        print('port: ' + str(pos))
+
+        pos = pos + 2
+        #SOF
+        message[pos] = 0xFF
+        message[pos + 1] = 0x45
+        print(len(message))
+        makeNetworkSettingMessage = message
+
+    def makeDHCPSettingMessage(HW_name, port):
+        message = bytes(38)
+        tempbuff = bytes()
+        protocol = bytes(10)
+
+        #SOF
+        message[0] = 0xFF
+        message[1] = 0x53
+
+        pos = 2
+        #Protocol version '0001'
+        protocol = StringToBytes(__Protocol_Ver)    # Protocol version length 4
+        message[2] = protocol[0]
+        message[3] = protocol[1]
+        message[4] = protocol[2]
+        message[5] = protocol[3]
+
+        pos = 6
+        #Data length
+        message[6] = 0
+        message[7] = 11
+
+        pos = 8
+        #Service Type
+        message[8] = 0x21   #Tool to device
+        #Servcie ID
+        message[9] = 0xA3   #set DHCP command
+
+        pos = 10
+        #Data get all the node info
+        tempbuff = StringToBytes(HW_name)   # HW Name length 8
+        print('HW Name' + str(len(tempbuff)))
+        for i in range(0, len(tempbuff) -2):
+            message[pos] = tempbuff[i]
+            pos = pos + 1
+
+        print('hw pos' + str(pos))
+        message[pos] = 0x25 # %
+        pos = pos + 1
+
+        message[pos] = port // 256
+        message[pos + 1] = port % 256
+        print('port: ' + str(pos))
+
+        pos = pos + 2
+        #SOF
+        message[pos] = 0xFF
+        message[pos + 1] = 0x45
+        print(len(message))
+        makeDHCPSettingMessage = message
+
+    def makeChangeDeviceNameMessage(Old_HW_Name, New_HW_Name):
+
+    def makeDeviceRTCMessage(HW_Name, RTCStr):
+
+    def makeSEMSIPSettingMessage(HW_Name, SEMSServerIP, port):
+
 
 #-----------------------------------------------------------------------------------#
 
--- SELCAM_to_Python/pythontest.py
+++ SELCAM_to_Python/pythontest.py
@@ -2,7 +2,11 @@ arr = [1,2,3,"11","22", 0xFF]
 
 dataPacket = [None] * len(arr)
 
+barr = bytes(11)
+
 #print(arr[5])
 
-for i in dataPacket:
+for i in arr:
     print(i)
+
+barr
